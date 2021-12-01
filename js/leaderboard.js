40b7d719774dd9221b88ef0334f1a076feb666f3
gyang-cpu <hyang105@my.bcit.ca>
2021-11-30 18:45:52 -0800

    removed error

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -11,7 +11,6 @@ const btn = document.getElementById(enter_btn);
 btn.addEventListener("click", loadLeaderboard);
 
 function loadLeaderboard() {
-<<<<<<< HEAD
     let amount = document.getElementById(lead).value;
     let params = "count=" + amount;
     xhttp.open("GET", endpoint, true);
@@ -19,15 +18,6 @@ function loadLeaderboard() {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
           let leader = document.getElementById(leaderboard);
-=======
-  let amount = document.getElementById(lead).value;
-  let params = "?count=" + amount;
-  xhttp.open("GET", endpoint, true);
-  xhttp.send(params);
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      let leader = document.getElementById(leaderboard);
->>>>>>> 71c26e91fc2f14f5550a1b68fdfb2b8a4b198c5c
 
       let info = JSON.parse(this.responseText);
       for (let i = 0; i < info.length; i++) {
