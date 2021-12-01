8c11ee4bce26acf767fc72dfe37dff33e25a761b
gyang-cpu <hyang105@my.bcit.ca>
2021-11-30 18:54:58 -0800

    changed leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -2,30 +2,29 @@ const xhttp = new XMLHttpRequest();
 const endpoint =
   "http://ec2-54-212-48-25.us-west-2.compute.amazonaws.com/API/v1/score/";
 
-const lead = "lead";
 const leaderboard = "leaderboard";
 const error_msg = "error_msg";
 
-const enter_btn = "enter_btn";
-const btn = document.getElementById(enter_btn);
-btn.addEventListener("click", loadLeaderboard);
 
 function loadLeaderboard() {
-    let amount = document.getElementById(lead).value;
-    let params = "count=" + amount;
+    let pullAmount = 10;
+    let params = "count=" + pullAmount;
     xhttp.open("GET", endpoint, true);
     xhttp.send(params);
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
           let leader = document.getElementById(leaderboard);
 
-      let info = JSON.parse(this.responseText);
-      for (let i = 0; i < info.length; i++) {
-        let new_li = document.createElement("li");
-        new_li.textContent =
-          "Username: " + info[i].username + " | " + "Score: " + info[i].score;
-        leader.appendChild(new_li);
-      }
-    }
-  };
+          let info = JSON.parse(this.responseText);
+          console.log(info)
+          for (let i = 0; i < info.length; i++) {
+            let new_li = document.createElement("li");
+            new_li.textContent =
+              "Username: " + info[i].username + " | " + "Score: " + info[i].score;
+            leader.appendChild(new_li);
+          }
+        }
+    };
 }
+
+loadLeaderboard()
